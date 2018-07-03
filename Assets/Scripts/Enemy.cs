d6bec386b6281414b9e5c44f61d52a3503527dc3
mrrrlisn <mrrrrlisin@gmail.com>
2018-07-03 13:50:23 +0300

    First Game

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10; // общедоступная переменная скорости
+    public float rotationSpeed = 500; // Cкорость вращения
+    public float movingDistance = 8; // До какого растояния дойдет обьект по X
+    public bool randomSpeed;
+    public float maxSpeed = 16;
+    
+
+	void Start ()
+    {
+        if (randomSpeed)
+        {
+            speed = Random.Range(1, maxSpeed);
+        }
+	}
+	
+	void Update ()
+    {
+        // Если положение обьекта больше равен movingDistance поворачиваем
+        if (transform.position.x >= movingDistance || transform.position.x <= -movingDistance) 
+        {
+            speed = -speed;
+        } 
+
+        //else
+        //{
+        //    Debug.Log("Я не поворачиваю");
+        //}
+        transform.Translate(speed * Time.deltaTime, 0, 0); // Передвигает объект
+        transform.Rotate(rotationSpeed * Time.deltaTime, 0, 0); // Вращает объект
+
+
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 6; // скорость ирока    
+    public float jumpSpeed = 50; // скорость прыжка
+    public float rotationSpeed = 10; // скорость прыжка
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        Move();
+	}
+
+    // Движение игрока
+    void Move()
+    {
+        // Получаем знаечние осей от -1 до 1 при нажатии кнопок управления и умножаем на скорость
+        float moveVertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float moveHorizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float moveUp = 0;  // движение вверх
+        float rotateHorizontal = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+        // при нажатии клавиши прыжка меняем значение moveUp на скорость прыжка
+        if (Input.GetButtonDown("Jump"))
+        {
+            moveUp = jumpSpeed  * Time.deltaTime;
+        }
+
+        transform.Rotate(0, rotateHorizontal, 0);
+        transform.Translate(moveHorizontal, moveUp, moveVertical); // перемещение
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
